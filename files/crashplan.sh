b874dfcde156d182bb814bca85ad0134d2fef499
David Woodward <2652498+David-Woodward@users.noreply.github.com>
2019-03-16 01:56:59 -0400

    Fix to gracefully handle invalid PUBLIC_IP values

--- files/crashplan.sh
+++ files/crashplan.sh
@@ -3,6 +3,11 @@
 # SIGTERM-handler
 term_handler() {
 
+    for headless_pid in $(ps -eo pid,cmd | grep -v grep | grep socat | awk '{print $1}'
+    do
+        kill "${headless_pid}"
+    done;
+
     # Stop crashplan
     /etc/init.d/crashplan stop
 
@@ -10,9 +15,23 @@ term_handler() {
 }
 
 # Kill logging (tail) and port proxy (socat) processes
-trap 'kill "$tail_pid"; \
-      for headless_pid in $(ps -eo pid,cmd | grep -v grep | grep socat | awk "{print \$1}"); do kill "${headless_pid}"; done; \
-      term_handler' INT QUIT KILL TERM
+trap 'kill "${tail_pid}"; term_handler' INT QUIT KILL TERM
+
+setupPortProxies() {
+    for addr in ${1}
+    do
+        if [ "${addr}" != '127.0.0.1' ] && [ "${addr}" != '::1' ]; then
+            echo "Creating a port proxy between ${addr}:${PUBLIC_PORT} and 127.0.0.1:${PUBLIC_PORT} ..."
+            if [ -z "${addr##*:*}" ]; then
+                socat TCP6-LISTEN:${PUBLIC_PORT},fork,bind=${addr} TCP:127.0.0.1:${PUBLIC_PORT} &
+            else
+                socat TCP4-LISTEN:${PUBLIC_PORT},fork,bind=${addr} TCP:127.0.0.1:${PUBLIC_PORT} &
+            fi
+            sleep .25
+            kill -0 $! 2>/dev/null && [ -z ${primary_ip} ] && primary_ip=${addr}
+        fi
+    done
+}
 
 /etc/init.d/crashplan start
 
@@ -24,22 +43,14 @@ done
 tail -n0 -F ${LOGS_FILES} &
 tail_pid=$!
 
-
 # Redirect 4244 traffic to localhost for CrashPlan 6 headless operation
 # Docker env PUBLIC_IP can be used to limit the listening addresses
-[ -z ${PUBLIC_IP} ] || [ "${PUBLIC_IP}" == '0.0.0.0' ] || [ "${PUBLIC_IP}" == '127.0.0.1' ] && PUBLIC_IP=$(ifconfig | sed -rn 's/.*inet\s+addr:\s*(\S+).*/\1/p')
-
-for addr in ${PUBLIC_IP}
-do
-  if [ "${addr}" != '127.0.0.1' ] && [ "${addr}" != '::1' ]; then
-    if [ -z "${addr##*:*}" ]; then
-      socat TCP6-LISTEN:${PUBLIC_PORT},fork,bind=${addr} TCP:127.0.0.1:${PUBLIC_PORT} &
-    else
-      socat TCP4-LISTEN:${PUBLIC_PORT},fork,bind=${addr} TCP:127.0.0.1:${PUBLIC_PORT} &
-    fi
-  fi
-done
-
+if [ -z ${PUBLIC_IP} ] || [ "${PUBLIC_IP}" == '0.0.0.0' ] || [ "${PUBLIC_IP}" == '127.0.0.1' ]; then
+    setupPortProxies "$(ifconfig | sed -rn 's/.*inet\s+addr:\s*(\S+).*/\1/p')"
+else
+    setupPortProxies "${PUBLIC_IP}"
+    [ -z ${primary_ip} ] && setupPortProxies "$(ifconfig | sed -rn 's/.*inet\s+addr:\s*(\S+).*/\1/p')"
+fi
 
 # wait "indefinitely"
 while [[ -e /proc/${tail_pid} ]]; do
