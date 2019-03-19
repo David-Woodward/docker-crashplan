b11c1e9c38ca443c84d1d621470a92ab3ea63e53
David Woodward <2652498+David-Woodward@users.noreply.github.com>
2019-03-18 22:19:18 -0400

    Added extra precautions to the process cleanup logic to prevent the process from trying to kill its own process/parent

--- files/cleanup.sh
+++ files/cleanup.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
 cleanup_procs() {
-    ps -eo pid,cmd --noheaders | grep -E "${1}" | grep -v -E '^\s*[0-9]+\s+(grep|ps)(\s|$)' | while read -r old_pid old_cmd
+    ps -eo pid,cmd --noheaders | grep -E "${1}" | grep -v -E '^\s*[0-9]+\s+(grep|ps|.*(/|\s)crashplan\.sh)(\s|$)' | while read -r old_pid old_cmd
     do
-        echo "Terminating process ${old_pid} - ${old_cmd}"
-        kill_counter=8
-        if [ ${old_pid} != 1 ] && kill ${old_pid}; then
-            while [ ${kill_counter} -gt 0 ] && old_s=$(ps -p ${old_pid} -o s) && [[ "${old_s}" && "${old_s}" != "Z" ]]
-            do
-                sleep .5
-                kill_counter=$(expr ${kill_counter} - 1)
-            done
-            [ ${kill_counter} == 0 ] && echo "Killing process ${old_pid} - ${old_cmd}" && kill -9 ${old_pid}
+        if [ ${old_pid} != 1 ]; then
+            echo "Terminating process ${old_pid} - ${old_cmd}"
+            kill_counter=8
+            if kill ${old_pid}; then
+                while [ ${kill_counter} -gt 0 ] && old_s=$(ps -p ${old_pid} -o s --noheaders) && [ ! -z ${old_s} ] && [ "${old_s}" != "Z" ]
+                do
+                    sleep .5
+                    kill_counter=$(expr ${kill_counter} - 1)
+                done
+                [ ${kill_counter} == 0 ] && echo "Killing process ${old_pid} - ${old_cmd}" && kill -9 ${old_pid}
+            fi
         fi
-done
+    done
 }
